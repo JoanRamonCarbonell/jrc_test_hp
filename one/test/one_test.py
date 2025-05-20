75934cc2206557bcd3be5a71699725b7df3c1d3e
joan ramon carbonell <joanramon.carbonell@wallbox.com>
2025-05-20 22:18:03 +0200

    Redo tests

--- one/test/one_test.py
+++ one/test/one_test.py
@@ -1,4 +1,3 @@
-# from __future__ import print_function   # to make print() work in python2 -> python2 not supported for Ubuntu 24.04
 import sys
 import os
 
@@ -6,50 +5,63 @@ import os
 # Compute the path to the build directory
 build_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'build'))
 
-# Print it for debugging
-print("Adding to sys.path:", build_path)
-
 # Add the build directory to the Python module search path
 sys.path.insert(0, build_path)
 
 import libone_bindings
 
-
-# Increasing functions returning double
-def increment(x):
-    return float(x + 1)
-
 def run_tests():
     print("Run tests with increasing functions...\n")
 
-    # 1.1 increment(x) == 6 in [0,10] -> x = 5
-    res = libone_bindings.find(increment, 6.0, 0, 10)
-    print("Test 1.1 - Range [0,10], increment(x) = 6:", res)
+    # print(libone_bindings.find_square(16.0, 0, 10))  # Should print 4
+    # print(libone_bindings.find_square(20.0, 0, 10))  # Should print -1
+
+    # 1.1 square(x) == 36 in [0,10] -> x = 6
+    res = libone_bindings.find_square(36.0, 0, 10)
+    print("Test 1.1 - Range [0,10], square(x) = 36 -> x = ", res)
+    assert res == 6
+
+    # 1.2 square(x) == 100 in [0,10] -> x = 100
+    res = libone_bindings.find_square(100.0, 0, 10)
+    print("Test 1.2 - Range [0,10], square(x) = 100 -> x = ", res)
+    assert res == 10
+
+    # 1.3 square(x) == 1 in [1,10] -> x = 1
+    res = libone_bindings.find_square(1.0, 1, 10)
+    print("Test 1.3 - Range [1,10], square(x) = 1 -> x = ", res)
+    assert res == 1
+
+    # 1.4 square(x) == 25 in [5,5] -> x = 5
+    res = libone_bindings.find_square(25.0, 5, 5)
+    print("Test 1.4 - Range [5,5], square(x) = 25 -> x = ", res)
     assert res == 5
 
-    # 1.2 increment(x) == 11 in [0,10] -> x = -1
-    res = libone_bindings.find(increment, 11.0, 0, 10)
-    print("Test 1.2 - Range [0,10], increment(x) = 11:", res)
+    # 1.5 square(x) == 1 in [2,10] -> x = -1
+    res = libone_bindings.find_square(1.0, 2, 10)
+    print("Test 1.5 - Range [2,10], square(x) = 1 -> x = ", res, " | Fail -> (1 < square(2))")
     assert res == -1
 
-    # 1.3 increment(x) == 4 in [5,10] -> x = -1
-    res = libone_bindings.find(increment, 4.0, 5, 10)
-    print("Test 1.3 - Range [5,10], increment(x) = 4:", res)
+    # 1.6 square(x) == 20 in [2,10] -> x = -1
+    res = libone_bindings.find_square(20.0, 2, 10)
+    print("Test 1.6 - Range [2,10], square(x) = 20 -> x = ", res, " | Fail -> (No index in range matches 20 in this increasing function)")
     assert res == -1
 
-    # 1.4 increment(x) == 4 in [-5,10] -> x = -1
-    res = libone_bindings.find(increment, 4.0, -5, 10)
-    print("Test 1.4 - Range [-5,10], increment(x) = 4:", res)
+    # 1.7 square(x) == 200 in [2,10] -> x = -1
+    res = libone_bindings.find_square(200.0, 2, 10)
+    print("Test 1.7 - Range [2,10], square(x) = 200 -> x = ", res, " | Fail -> (200 > square(10))")
     assert res == -1
 
-    # 1.5 increment(x) == 4 in [10,0] -> x = -1
-    res = libone_bindings.find(increment, 4.0, 10, 0)
-    print("Test 1.5 - Range [10,0], increment(x) = 4:", res)
+    # 1.8 square(x) == 36 in [10,0] -> x = -1
+    res = libone_bindings.find_square(36.0, 10, 0)
+    print("Test 1.8 - Range [10,0], square(x) = 36 -> x = ", res, " | Fail -> (a > b)")
     assert res == -1
 
+    # 1.9 square(x) == 36 in [-10,10] -> x = -1
+    res = libone_bindings.find_square(36.0, -10, 10)
+    print("Test 1.9 - Range [-10,10], square(x) = 36 -> x = ", res, " | Fail -> (a < 0)")
+    assert res == -1
 
     print("\nAll tests passed.")
-
 
 if __name__ == '__main__':
     run_tests()
